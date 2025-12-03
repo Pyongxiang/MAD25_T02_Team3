1e6fee3ca711ddcad59a148b801c6919a492fa56
Pyongxiang <s10258483@connect.np.edu.sg>
2025-12-03 16:21:45 +0800

    Updated firebase dependencies, corrected it to the new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
     id("com.google.gms.google-services")//google services gradle plugin
 }
 
@@ -63,9 +62,8 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))// firebase BoM
 
-    implementation("com.google.firebase:firebase-auth-ktx")// Firebase Authentication
-
-    implementation("com.google.firebase:firebase-firestore-ktx")// Cloud Firestore
+    implementation("com.google.firebase:firebase-auth")// Firebase Authentication
+    implementation("com.google.firebase:firebase-firestore")// Cloud Firestore
 
     implementation("androidx.activity:activity-compose:1.9.0") // Jetpack Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
